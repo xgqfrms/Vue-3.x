6201f233fdb6276bb8544eef5d9dc2a59599c996
xgqfrms <xgqfrms@outlook.com>
2021-01-20 22:17:12 +0800

    vue 3.x
    https://www.vuemastery.com/courses/intro-to-vue-3/forms-and-v-model-vue3

---
+++ src/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <a href="https://www.vuemastery.com/courses/intro-to-vue-3/forms-and-v-model-vue3">https://www.vuemastery.com/courses/intro-to-vue-3/forms-and-v-model-vue3</a>
+  </div>
+</template>
